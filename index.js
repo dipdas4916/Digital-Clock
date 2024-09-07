f1b3780065a20a864d1a834082e34e6cd634836c
dipdas4916 <dasdip4916@gmail.com>
2024-09-07 20:48:03 +0530

    Digital Clock Project Successfully Uploaded

---
+++ index.js
@@ -0,0 +1,34 @@
+const time = document.getElementById('time');
+
+setInterval(function(){
+    const date = new Date();
+    let day = date.getDay();
+    switch(day){
+        case 1:
+            day = 'Monday';
+            break;
+        case 1:
+            day = 'Tuesday';
+            break;
+        case 1:
+            day = 'Wednesday';
+            break;
+        case 4:
+            day = 'Thrusday';
+            break;
+        case 5:
+            day = 'Friday';
+            break;
+        case 6:
+            day = 'Saturday';
+            break;
+        case 7:
+            day = 'Sunday';
+            break;
+        default:
+            day = 'Sunday';
+            break;
+            
+    }
+    time.innerHTML = `<span>Date: ${date.toLocaleDateString()}<br>Day: ${day}<br>Time: ${date.toLocaleTimeString()}</span> `;
+}, 1000) // It shows after 1000ms interval the function runs, until it stopped. And it is the only syntax.
